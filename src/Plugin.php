cbeb3a712f025ac29e04ee0d083380095a07c07f
Mark Harding <mark@kramnorth.com>
2018-01-02 16:15:48 +0000

    (feat): allow to be used an a plugin

---
+++ src/Plugin.php
@@ -0,0 +1,14 @@
+<?php
+namespace MW3;
+
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
+
+class Plugin implements PluginInterface
+{
+    public function activate(Composer $composer, IOInterface $io)
+    {
+        exec("npm install");
+    }
+}
